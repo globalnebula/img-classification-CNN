a19bfdc8f0cb16486c3fb0b18a8e2a6d7667bf39
globalnebula <kunalachintya@gmail.com>
2024-01-25 02:18:22 +0530

    fixing tensorflow and keras import issues

--- img_classifier.py
+++ img_classifier.py
@@ -1,7 +1,5 @@
-# Install the required libraries
-# pip install tensorflow scikit-learn
-
 import tensorflow as tf
+from tensorflow import keras
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
